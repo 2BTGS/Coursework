91eaade2a7f3e0471ff9f8aecee55b455f1b2c00
Bader <psybm13@nottingham.ac.uk>
2025-12-04 02:30:22 +0000

    test

--- rpn.c
+++ rpn.c
@@ -1,7 +1,33 @@
 #include "rpn.h"
-
 #include "stack.h"
 #include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int is_operator(char c)
+{
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+int get_precedence(char c)
+{
+    switch (c) {
+        case '^': return 3;
+        case '*':
+        case '/': return 2;
+        case '+':
+        case '-': return 1;
+        default: return 0;
+    }
+}
+
+int is_left_associative(char c)
+{
+    return c != '^';
+}
 
 /* convert an infix expression to postfix (rpn) using the */
 /* shunting yard algorithm. */
@@ -9,9 +35,113 @@
 /* if an error occurs during evaluation, return silently with NULL. */
 Queue *infix_to_postfix(char *expr)
 {
-	/* this is just a placeholder return */
-	/* you will need to replace it */
-	return NULL;
+    Queue *queue = initialise_queue();
+    Stack *stack = initialise_stack();
+    int i = 0;
+    int j = 0;
+    int expecting_operand = 1;
+    char *top_op_str;
+    char current_op_str[2];
+    char num_buf[64];
+    int found_paren;
+
+    if (!queue || !stack) {
+        return NULL;
+    }
+
+    while (expr[i]) {
+        if (expr[i] == ' ') {
+            i++;
+            continue;
+        }
+
+        if (isdigit(expr[i]) || expr[i] == '.' ||
+           ((expr[i] == '+' || expr[i] == '-') && expecting_operand)) {
+
+            j = 0;
+            if (expr[i] == '+' || expr[i] == '-') {
+                num_buf[j++] = expr[i++];
+            }
+
+            while (isdigit(expr[i]) || expr[i] == '.') {
+                if (j < 63) num_buf[j++] = expr[i];
+                i++;
+            }
+            num_buf[j] = '\0';
+
+            if (j == 1 && (num_buf[0] == '+' || num_buf[0] == '-')) {
+                free_queue(queue);
+                free_stack(stack);
+                return NULL;
+            }
+
+            push_queue(queue, num_buf, j + 1);
+            expecting_operand = 0;
+            continue;
+        }
+
+        if (expr[i] == '(') {
+            push_stack(stack, "(", 2);
+            expecting_operand = 1;
+            i++;
+        }
+        else if (expr[i] == ')') {
+            found_paren = 0;
+            while ((top_op_str = (char *)peek_stack(stack)) != NULL) {
+                if (strcmp(top_op_str, "(") == 0) {
+                    free(pop_stack(stack));
+                    found_paren = 1;
+                    break;
+                }
+                push_queue(queue, top_op_str, strlen(top_op_str) + 1);
+                free(pop_stack(stack));
+            }
+            if (!found_paren) {
+                free_queue(queue);
+                free_stack(stack);
+                return NULL;
+            }
+            expecting_operand = 0;
+            i++;
+        }
+        else if (is_operator(expr[i])) {
+            while ((top_op_str = (char *)peek_stack(stack)) != NULL) {
+                if (strcmp(top_op_str, "(") == 0) break;
+
+                if ((get_precedence(top_op_str[0]) > get_precedence(expr[i])) ||
+                    (get_precedence(top_op_str[0]) == get_precedence(expr[i]) && is_left_associative(expr[i]))) {
+                    push_queue(queue, top_op_str, 2);
+                    free(pop_stack(stack));
+                } else {
+                    break;
+                }
+            }
+            current_op_str[0] = expr[i];
+            current_op_str[1] = '\0';
+            push_stack(stack, current_op_str, 2);
+            expecting_operand = 1;
+            i++;
+        }
+        else {
+            free_queue(queue);
+            free_stack(stack);
+            return NULL;
+        }
+    }
+
+    while ((top_op_str = (char *)peek_stack(stack)) != NULL) {
+        if (strcmp(top_op_str, "(") == 0) {
+            free(pop_stack(stack));
+            free_queue(queue);
+            free_stack(stack);
+            return NULL;
+        }
+        push_queue(queue, top_op_str, 2);
+        free(pop_stack(stack));
+    }
+
+    free_stack(stack);
+    return queue;
 }
 
 /* evaluate the rpn expression given in *queue. */
@@ -20,7 +150,71 @@ Queue *infix_to_postfix(char *expr)
 /* assume a precision of eight decimal places when performing arithmetic. */
 double evaluate_rpn(Queue *queue)
 {
-	/* this is just a placeholder return */
-	/* you will need to replace it */
-	return 0.0;
+    Stack *stack = initialise_stack();
+    char *token;
+    double *val1_ptr, *val2_ptr, *res_ptr;
+    double v1, v2, res_val;
+    char *endptr;
+
+    if (!queue || !stack) {
+        return HUGE_VAL;
+    }
+
+    while ((token = (char *)pop_queue(queue)) != NULL) {
+
+        if (is_operator(token[0]) && strlen(token) == 1) {
+            val2_ptr = (double *)pop_stack(stack);
+            val1_ptr = (double *)pop_stack(stack);
+
+            if (!val1_ptr || !val2_ptr) {
+                if (val2_ptr) free(val2_ptr);
+                if (val1_ptr) free(val1_ptr);
+                free(token);
+                free_stack(stack);
+                return HUGE_VAL;
+            }
+
+            v1 = *val1_ptr;
+            v2 = *val2_ptr;
+            free(val1_ptr);
+            free(val2_ptr);
+
+            switch (token[0]) {
+                case '+': res_val = v1 + v2; break;
+                case '-': res_val = v1 - v2; break;
+                case '*': res_val = v1 * v2; break;
+                case '/': res_val = v1 / v2; break;
+                case '^': res_val = pow(v1, v2); break;
+                default: res_val = 0.0; break;
+            }
+            push_stack(stack, &res_val, sizeof(double));
+
+        } else {
+            res_val = strtod(token, &endptr);
+            if (endptr == token) {
+                free(token);
+                free_stack(stack);
+                return HUGE_VAL;
+            }
+            push_stack(stack, &res_val, sizeof(double));
+        }
+        free(token);
+    }
+
+    res_ptr = (double *)pop_stack(stack);
+    if (!res_ptr) {
+        free_stack(stack);
+        return HUGE_VAL;
+    }
+
+    res_val = *res_ptr;
+    free(res_ptr);
+
+    if (peek_stack(stack) != NULL) {
+        free_stack(stack);
+        return HUGE_VAL;
+    }
+
+    free_stack(stack);
+    return res_val;
 }
--- sort.c
+++ sort.c
@@ -1,4 +1,6 @@
 #include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /* sort linked list *list using merge insertion sort. */
 /* upon success, the elements in *list will be sorted. */
@@ -8,6 +10,53 @@
 /* less than, equal to, or greater than second argument respectively. */
 void insertion_sort(LinkedList *list, int (*compare)(void *, void *))
 {
+    Node *sorted_head = NULL;
+    Node *sorted_tail = NULL;
+    Node *current = NULL;
+    Node *next_node = NULL;
+    Node *search = NULL;
+
+    if (list == NULL || list->head == NULL || list->head->next == NULL) {
+        return;
+    }
+
+    current = list->head;
+
+    while (current != NULL) {
+        next_node = current->next;
+
+        current->next = NULL;
+        current->prev = NULL;
+
+        if (sorted_head == NULL) {
+            sorted_head = current;
+            sorted_tail = current;
+        } else if (compare(current->data, sorted_head->data) < 0) {
+            current->next = sorted_head;
+            sorted_head->prev = current;
+            sorted_head = current;
+        } else {
+            search = sorted_head;
+            while (search != NULL && compare(search->data, current->data) <= 0) {
+                search = search->next;
+            }
+
+            if (search == NULL) {
+                sorted_tail->next = current;
+                current->prev = sorted_tail;
+                sorted_tail = current;
+            } else {
+                current->next = search;
+                current->prev = search->prev;
+                search->prev->next = current;
+                search->prev = current;
+            }
+        }
+        current = next_node;
+    }
+
+    list->head = sorted_head;
+    list->tail = sorted_tail;
 }
 
 /* sort linked list *list using merge sort algorithm. */
@@ -18,4 +67,5 @@ void insertion_sort(LinkedList *list, int (*compare)(void *, void *))
 /* less than, equal to, or greater than second argument respectively. */
 void merge_sort(LinkedList *list, int (*compare)(void *, void *))
 {
+
 }
