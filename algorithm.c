5e4c6de03cb2ff6d4ce71f1f754b8907d87b706a
Bader <psybm13@nottingham.ac.uk>
2025-12-04 23:37:29 +0000

    IloveC

--- algorithm.c
+++ algorithm.c
@@ -1,6 +1,47 @@
 #include "algorithm.h"
+#include <stdlib.h>
+#include <string.h>
 
 /* algorithm implementation */
 char *algorithm(char *input)
 {
+    char *result;
+    int len, i;
+    char c;
+
+    if (input == NULL) {
+        return NULL;
+    }
+
+    len = strlen(input);
+    result = malloc((len + 1) * sizeof(char));
+
+    if (result == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < len; i++) {
+        c = input[i];
+
+        if (c == ' ') {
+            c = 'h';
+        } else if (c == 'h') {
+            c = ' ';
+        } else if (c == ',') {
+            c = 'i';
+        } else if (c >= '0' && c <= '9') {
+            c = c + 58;
+        } else if (c >= 'a' && c <= 'z') {
+            if (c <= 'm') {
+                c = c + 19;
+            } else {
+                c = c - 19;
+            }
+        }
+
+        result[i] = c;
+    }
+
+    result[len] = '\0';
+    return result;
 }
--- sort.c
+++ sort.c
@@ -2,6 +2,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static Node *split_list(Node *head)
+{
+    Node *fast = head;
+    Node *slow = head;
+    Node *temp;
+
+    if (head == NULL || head->next == NULL) {
+        return NULL;
+    }
+
+    while (fast->next != NULL && fast->next->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    temp = slow->next;
+    slow->next = NULL;
+    if (temp != NULL) {
+        temp->prev = NULL;
+    }
+
+    return temp;
+}
+
+static Node *merge_lists(Node *first, Node *second, int (*compare)(void *, void *))
+{
+    if (first == NULL) return second;
+    if (second == NULL) return first;
+
+    if (compare(first->data, second->data) <= 0) {
+        first->next = merge_lists(first->next, second, compare);
+        if (first->next != NULL) {
+            first->next->prev = first;
+        }
+        first->prev = NULL;
+        return first;
+    } else {
+        second->next = merge_lists(first, second->next, compare);
+        if (second->next != NULL) {
+            second->next->prev = second;
+        }
+        second->prev = NULL;
+        return second;
+    }
+}
+
+static Node *merge_sort_recursive(Node *head, int (*compare)(void *, void *))
+{
+    Node *second_half;
+
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    second_half = split_list(head);
+
+    head = merge_sort_recursive(head, compare);
+    second_half = merge_sort_recursive(second_half, compare);
+
+    return merge_lists(head, second_half, compare);
+}
+
 /* sort linked list *list using merge insertion sort. */
 /* upon success, the elements in *list will be sorted. */
 /* return silently if *list is uninitialised or empty. */
@@ -67,5 +129,19 @@ void insertion_sort(LinkedList *list, int (*compare)(void *, void *))
 /* less than, equal to, or greater than second argument respectively. */
 void merge_sort(LinkedList *list, int (*compare)(void *, void *))
 {
+    Node *current;
 
+    if (list == NULL || list->head == NULL || list->head->next == NULL) {
+        return;
+    }
+
+    list->head = merge_sort_recursive(list->head, compare);
+
+    current = list->head;
+    if (current != NULL) {
+        while (current->next != NULL) {
+            current = current->next;
+        }
+    }
+    list->tail = current;
 }
